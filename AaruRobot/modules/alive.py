b2c35e51b20bcff20323f9bb9c1695e61e2089c6
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2023-01-16 08:13:19 -0800

    Update alive.py

--- AaruRobot/modules/alive.py
+++ AaruRobot/modules/alive.py
@@ -9,21 +9,21 @@ from AaruRobot import telethn as tbot
 
 
 PHOTO = [
-    "https://telegra.ph/file/7c1ee32ae45ea4a9c106a.jpg",
-    "https://telegra.ph/file/7c1ee32ae45ea4a9c106a.jpg",
+    "https://te.legra.ph/file/fdf0fe6a94d55dc2f137f.jpg",
+    "https://te.legra.ph/file/fdf0fe6a94d55dc2f137f.jpg",
 ]
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ  ✘ ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [⏤͟͟͞͞x𝐃🥀| 𓆩 𝐂𝐎𝐃𝐄𝐑 𓆪 |∘𖣘︎⃞⃟🔥°](https://t.me/ll_ll_LegendHacker_IN_ll_ll)** \n\n"
+  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [⏤͟͟͞͞x𝐃🥀| 𓆩 𝐂𝐎𝐃𝐄𝐑 𓆪 |∘𖣘︎⃞⃟🔥°](https://t.me/NOMRExd)** \n\n"
   TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
   BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", f"https://t.me/@ll_Zain_Ro_ll_Bot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/Zain_Logo_Meker"),
+            Button.url("ʜᴇʟᴘ​", f"https://t.me/AVADHUTMNGR_bot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/NOMRExd"),
         ]
     ]
   ran = random.choice(PHOTO)
